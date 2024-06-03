ec9b6cabef3cf2dccce45ffb539fabf2f76ba1ef
Willem April <90314284+Willie3011@users.noreply.github.com>
2024-06-03 17:25:12 +0200

    Created a header and used react router for routing

--- the-kitchen/src/App.jsx
+++ the-kitchen/src/App.jsx
@@ -1,11 +1,57 @@
 import { useState } from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Cart from './Pages/Cart/Cart';
+import Menu from './Pages/Menu/Menu';
+import Reviews from './Pages/Reviews/Reviews';
+import Layout from './Components/Layout/Layout';
 function App() {
-  return (
-    <>
-      <h1 className='text-green-500 font-thin'>Hello World</h1>
-    </>
-  )
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (<Layout>
+        <Home/>
+        </Layout>
+      ),
+      errorElement: <div>404: Page Not Found</div>
+    },
+    {
+      path: '/about',
+      element: (
+        <Layout>
+          <About />
+        </Layout>
+      ),
+    },
+    {
+      path: '/menu',
+      element: (
+        <Layout>
+          <Menu />
+        </Layout>
+      ),
+    },
+    {
+      path: '/reviews',
+      element: (
+        <Layout>
+          <Reviews />
+        </Layout>
+      ),
+    },
+    {
+      path: '/cart',
+      element: (
+        <Layout>
+          <Cart />
+        </Layout>
+      ),
+    }
+]);
+  return <RouterProvider router={router}/>
+  
 }
 
 export default App
---
+++ the-kitchen/src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
+
+function Home() {
+  return (
+    <div className='bg-black'>
+        
+    </div>
+  )
+}
+
+export default Home
